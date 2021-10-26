09419d144bc0f1130ad1736ea7cb66d2d415df32
sxuet <1065464173@qq.com>
2021-10-26 13:31:55 +0800

    push
    Signed-off-by: sxuet <1065464173@qq.com>

--- src/simple/FindMode501.java
+++ src/simple/FindMode501.java
@@ -2,6 +2,9 @@ package simple;
 
 import struct.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Sxuet
  * @program Javasource_leetcode
@@ -11,8 +14,67 @@ import struct.TreeNode;
  * @since 2021-10-26 11:26
  */
 public class FindMode501 {
+  /**
+   * Morris 遍历的实质就是避免用栈结构,具体是通过让底层节点指向 null 的空闲指针指回上层的某个节点，从而完成下层到上层的移动。
+   *
+   * <p>Morris 遍历的大体流程： <br>
+   * 我们记当前节点的指针为 cur，如果 cur 所指向的节点没有左孩子，那么 cur 指针向右移动，即 cur = cur.right。 <br>
+   * 如果 cur 所指向的节点有左孩子，那么先找到 cur 左子树的最右的节点，并使用 mostright 指针指向该最右节点。 1）如果 mostright 所指向的节点的 right
+   * 指针为空，那么让mostright 的 right 指针指向 cur，然后cur 指针向左移动； 2）如果 mostright 所指向的节点的 right 指向 cur，那么让 right
+   * 重新指向空，然后 cur 向右移动。
+   *
+   * <p>执行用时： 2 ms , 在所有 Java 提交中击败了 40.79% 的用户 内存消耗： 39 MB , 在所有 Java 提交中击败了 33.77% 的用户
+   */
+  int base, count, maxCount;
+
+  List<Integer> answer = new ArrayList<Integer>();
+
   public int[] findMode(TreeNode root) {
-    return null;
+    // 初始化
+    TreeNode cur = root, pre = null;
+    // 当前节点不为空时，找到其左子树的最右节点，使其右节指向当前节点
+    while (cur != null) {
+      if (cur.left == null) {
+        update(cur.val);
+        cur = cur.right;
+        continue;
+      }
+      // 找到其左子树的最右节点，使其右节指向当前节点
+      pre = cur.left;
+      while (pre.right != null && pre.right != cur) {
+        pre = pre.right;
+      }
+      if (pre.right == null) {
+        pre.right = cur;
+        cur = cur.left;
+      } else {
+        pre.right = null;
+        update(cur.val);
+        cur = cur.right;
+      }
+    }
+    int[] mode = new int[answer.size()];
+    for (int i = 0; i < answer.size(); ++i) {
+      mode[i] = answer.get(i);
+    }
+    return mode;
+  }
+
+  public void update(int x) {
+    if (x == base) {
+      ++count;
+    } else {
+      count = 1;
+      base = x;
+    }
+    if (count == maxCount) {
+      answer.add(base);
+    }
+    if (count > maxCount) {
+      maxCount = count;
+      answer.clear();
+      answer.add(base);
+    }
   }
 }
 /*
