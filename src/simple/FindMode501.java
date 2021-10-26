a1f65beeafd0615fdc67ac8e0f448a876ec84d18
sxuet <1065464173@qq.com>
2021-10-26 13:04:22 +0800

    push
    Signed-off-by: sxuet <1065464173@qq.com>

---
+++ src/simple/FindMode501.java
@@ -0,0 +1,32 @@
+package simple;
+
+import struct.TreeNode;
+
+/**
+ * @author Sxuet
+ * @program Javasource_leetcode
+ * @description 给定一个有相同值的二叉搜索树（BST），找出BST中的所有众数（出现频率最高的元素）。 假定BST有如下定义：
+ *     。结点左子树中所含结点的值小于等于当前结点的值。结点右子树中所含结点的值大于等于当前结点的值，左子树和右子树都是二叉搜索树 例如：
+ *     给定BST【1，null，2，2】，返回【2】.如果众数超过1个，不需考虑输出顺序
+ * @since 2021-10-26 11:26
+ */
+public class FindMode501 {
+  public int[] findMode(TreeNode root) {
+    return null;
+  }
+}
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
