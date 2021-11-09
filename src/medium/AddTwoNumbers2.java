f3251d8f5eab97756cd6cda025950111029dc0ef
sxuet <1065464173@qq.com>
2021-11-09 11:01:09 +0800

    2

---
+++ src/medium/AddTwoNumbers2.java
@@ -0,0 +1,46 @@
+package medium;
+
+import struct.ListNode;
+
+/**
+ * @author Sxuet
+ * @program Javasource_leetcode
+ * @description 给你两个非空的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+ *     请你将两个数相加，并以相同形式返回一个表示和的链表。你可以假设除了数字0之外，这两个数都不会以0开头。
+ * @since 2021-11-09 10:33
+ */
+public class AddTwoNumbers2 {
+  /**
+   * 一位一位的加，并记录进位<br>
+   * 执行用时： 2 ms , 在所有 Java 提交中击败了 93.16% 的用户 内存消耗： 38.7 MB , 在所有 Java 提交中击败了 47.41% 的用户
+   *
+   * @param l1
+   * @param l2
+   * @return
+   */
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode ans = new ListNode();
+    ListNode node = ans;
+    // 进位
+    int c1 = 0;
+    while (l1 != null || l2 != null) {
+      int i = 0;
+      if (l1 != null) {
+        i += l1.val;
+        l1 = l1.next;
+      }
+      if (l2 != null) {
+        i += l2.val;
+        l2 = l2.next;
+      }
+      i += c1;
+      node.next = new ListNode(i % 10);
+      c1 = i / 10;
+      node = node.next;
+    }
+    if (c1 > 0) {
+      node.next = new ListNode(c1);
+    }
+    return ans.next;
+  }
+}
---
+++ src/simple/FindTilt563.java
@@ -0,0 +1,28 @@
+package simple;
+
+import struct.TreeNode;
+
+/**
+ * @author Sxuet
+ * @program Javasource_leetcode
+ * @description 给定一个二叉树，计算整个树的坡度。
+ *     一个树的节点的坡度定义即为，该节点左子树的节点之和和右子树节点之和的差的绝对值。如果没有左子树的话，左子树的节点之和为0；没有右子树的话也是一样。空结点的坡度是0。
+ *     整个树的坡度就是其所有节点的坡度之和。
+ * @since 2021-11-09 09:44
+ */
+public class FindTilt563 {
+  int tilt = 0;
+  /** 执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗： 38.2 MB , 在所有 Java 提交中击败了 88.82% 的用户 */
+  public int findTilt(TreeNode root) {
+    traverse(root);
+    return tilt;
+  }
+
+  public int traverse(TreeNode root) {
+    if (root == null) return 0;
+    int left = traverse(root.left);
+    int right = traverse(root.right);
+    tilt += Math.abs(left - right);
+    return left + right + root.val;
+  }
+}
